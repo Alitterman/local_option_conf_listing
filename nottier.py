694028352c6523a133c90dda6cacf2108fbe3295
Alitterman <43596801+Alitterman@users.noreply.github.com>
2022-08-18 10:16:06 +0800

    add
    添加gitignore

--- nottier.py
+++ nottier.py
@@ -1,32 +1,34 @@
-class Nottier():
+import abc
+
+
+class Nottier:
+    @abc.abstractmethod
     def __init__(self):
-        self.status 
+        self.status
         pass
 
-    # def __new__(self):
-    #     raise "1"
-
+    @abc.abstractmethod
     def callback(self, args):
         pass
 
+
 class A(Nottier):
-    def __init_(self,name):
-        super(A,self).__init_(name)
-        self.status = 0
+    def __init_(self, name):
+        super(A, self).__init__(name)
+        status = 0
         print("123")
         pass
-    
-    def __new__(self,name):
+
+    def __new__(self, name):
         print("实例A开始创建")
         return self
 
-    def callback(self,*args):
+    def callback(self, *args):
         # super().callback(args)
         print("hello,world!")
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     B = A("bb")
 
     B.callback("a")
-
-
